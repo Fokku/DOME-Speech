dcdf211a3210476e432514ca8e729c092218fe4e
Kai yuan <43857854+Fokku@users.noreply.github.com>
2021-01-20 10:21:07 +0800

    Adding recorder class

---
+++ recorder/main.py
@@ -0,0 +1,62 @@
+
+"""
+import base64
+import mysql.connector
+from mysql.connector import errorcode
+
+# Obtain connection string information from the portal
+config = {
+  'host':'c200dome.database.windows.net',
+  'user':'c200dome123@c200dome',
+  'password':'bakedbean12!',
+  'database':'Speaker-recognition',
+  'client_flags': [ClientFlag.SSL],
+  'ssl_cert': '/var/wwww/html/DigiCertGlobalRootG2.crt.pem'
+}
+
+# Construct connection string
+try:
+   conn = mysql.connector.connect(**config)
+   print("Connection established")
+except mysql.connector.Error as err:
+  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    print("Something is wrong with the user name or password")
+  elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    print("Database does not exist")
+  else:
+    print(err)
+else:
+  cursor = conn.cursor()
+
+  # Drop previous table of same name if one exists
+  cursor.execute("DROP TABLE IF EXISTS inventory;")
+  print("Finished dropping table (if existed).")
+
+  # Create table
+  cursor.execute("CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);")
+  print("Finished creating table.")
+
+  # Insert some data into table
+  cursor.execute("INSERT INTO inventory (name, quantity) VALUES (%s, %s);", ("banana", 150))
+  print("Inserted",cursor.rowcount,"row(s) of data.")
+  cursor.execute("INSERT INTO inventory (name, quantity) VALUES (%s, %s);", ("orange", 154))
+  print("Inserted",cursor.rowcount,"row(s) of data.")
+  cursor.execute("INSERT INTO inventory (name, quantity) VALUES (%s, %s);", ("apple", 100))
+  print("Inserted",cursor.rowcount,"row(s) of data.")
+
+  # Cleanup
+  conn.commit()
+  cursor.close()
+  conn.close()
+  print("Done.")
+
+
+
+with open(r"D:\School files\C200\audio\9.wav", "rb").read() as data:
+    encoded = base64.b64encode(data)
+
+print(encoded)
+
+with open("decoded.wav", "wb") as fh:
+    fh.write(base64.decodebytes(encoded))
+"""
